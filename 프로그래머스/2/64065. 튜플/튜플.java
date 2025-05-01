fd808a57bf454b300c4abe997213a8858a842da0
Kim <63275665+devkimc@users.noreply.github.com>
2025-05-01 16:40:05 +0900

    [level 2] Title: 튜플, Time: 65.52 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,32 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        List<Integer> answerList = new ArrayList<>();
+        Set<String> answerSet = new LinkedHashSet<>();
         
         // 시작과 끝의 {{, }} 제거
         String[] tupleArray = s.substring(2, s.length() - 2) 
                                .split("\\},\\{");
         
+        Arrays.sort(tupleArray, ((a, b) -> a.length() - b.length()));
+        
         // 튜플의 원소의 개수만큼 루프를 돌기 위함
-        for(String tupleElementIncludedComma: tupleArray) {
+        for(String tupleElementArray: tupleArray) {
             
-            // 다음 원소를 찾아야 함
-            for(String tupleElementIncludedCommaForAnswer: tupleArray) {
-                int[] tupleElementArray = Arrays.stream(tupleElementIncludedCommaForAnswer.split(","))
-                                                .mapToInt(Integer::parseInt)
-                                                .toArray();
-                
-                // 튜플 원소의 길이 == answer 길이 + 1 일 경우, 다음 원소임
-                if(tupleElementArray.length == answerList.size() + 1) {
-                    
-                    // 다음 원소 = 튜플 원소의 합 - answer의 합
-                    int nextElement = Arrays.stream(tupleElementArray).sum() - answerList.stream().mapToInt(i->i).sum();
-                    answerList.add(nextElement);
-                    break;
-                }
+            // 다음 원소를 찾았을 경우 break
+            for(String element: tupleElementArray.split(",")) {
+                if(answerSet.add(element)) break;
             }
         }
         
-        return answerList.stream().mapToInt(i->i).toArray();
+        return answerSet.stream().mapToInt(Integer::parseInt).toArray();
     }
 }
