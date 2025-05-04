303b5e645f8b148fb66bec07972721abdb5e5c5c
Kim <63275665+devkimc@users.noreply.github.com>
2025-05-04 14:33:09 +0900

    [level 2] Title: 롤케이크 자르기, Time: 243.59 ms, Memory: 191 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] topping) {
+        int answer = 0;
+        
+        Map<Integer, Integer> cheolCake = new HashMap<>();
+        Map<Integer, Integer> broCake = new HashMap<>();
+        
+        for(int i = 0; i < topping.length; i++) {
+            broCake.put(topping[i], broCake.getOrDefault(topping[i], 0) + 1);
+        }
+        
+        for(int i = 0; i < topping.length; i++) {
+            cheolCake.put(topping[i], broCake.getOrDefault(topping[i], 0) + 1);
+            
+            if(broCake.get(topping[i]) == 1) {
+                Integer rm = broCake.remove(topping[i]);
+            } else {
+                broCake.put(topping[i], broCake.getOrDefault(topping[i], 0) - 1);
+            }
+            
+            if(cheolCake.size() == broCake.size()) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
